226fb1f884f48b34c97df5dd53edbf2ebc00a0f1
pawgrmr <27719504+pawgrmr@users.noreply.github.com>
2018-12-14 11:11:11 +1100

    minor changes to previous exercises

--- ruby/two-fer/two_fer_test.rb
+++ ruby/two-fer/two_fer_test.rb
@@ -1,11 +1,10 @@
 require 'minitest/autorun'
 require_relative 'two_fer'
-require 'minitest/pride'
 
 # Common test data version: 1.2.0 4fc1acb
 class TwoFerTest < Minitest::Test
   def test_no_name_given
-    #skip
+    # skip
     assert_equal "One for you, one for me.", TwoFer.two_fer
   end
 
