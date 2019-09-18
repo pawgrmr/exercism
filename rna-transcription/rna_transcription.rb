dba395f952e9c18da12d9254428e0dee0f22a800
pawgrmr <27719504+pawgrmr@users.noreply.github.com>
2019-09-18 20:40:22 +1000

    RNA transcription side exercise

---
+++ rna-transcription/rna_transcription.rb
@@ -0,0 +1,14 @@
+class Complement
+
+SEQUENCES = {
+  'C' => 'G',
+  'G' => 'C',
+  'T' => 'A',
+  'A' => 'U',
+  ''  => ''
+}
+  def self.of_dna(sequences) 
+    result = sequences.each_char.map{|sequence| SEQUENCES.fetch(sequence)}.join
+    p result
+  end
+end
