55b93bc5749dc43702b39912e98468540cea2c04
pawgrmr <27719504+pawgrmr@users.noreply.github.com>
2019-09-19 17:09:28 +1000

    pangram completed

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,25 +1,16 @@
 class Pangram
   def self.pangram?(args)
-     # p args.include?()
-    
-
-     # args.split('').reject{|x| x == ''}.join.scan(/[a-z]/)
-     no_space = args.delete(' ')
-
-    if no_space.scan(/[a-z]/).join === ''
-      return false
-    else
-      return true
-    end
-    
-      # result = true if result.scan(/[a-z]/) // fails #1, passes #2, #3
-
+  ('a'..'z').all?{|x| args.downcase.include?(x)}
   end
 end
 
-    sentence = 'abcdefghijklmnopqrstuvwxyz'
-    result = Pangram.pangram?(sentence)
-    p "Expected true, got: #{result.inspect}. #{sentence.inspect} IS a pangram"
+    # result = Pangram.pangram?(sentence)
+    # p "Expected false, got: #{result.inspect}. #{sentence.inspect} is NOT a pangram"
+
+     # p args.include?()
+    
+     # args.split('').reject{|x| x == ''}.join.scan(/[a-z]/)
+     # no_space = sentence.delete(' ').scan(/[a-z]/)
 
 # => ["t", "h", "e", "q", "u", "i", "c", "k", "b", "r", "o", "w", "n", "f", "o", "x", "j", "u", "m", "p", "s", "o", "v", "e", "r", "t", "h", "e", "l", "a", "z", "y", "d", "o", "g"]
 
@@ -30,4 +21,14 @@ end
 
  #loop over each object in the array and match it with alphabet a-zA-Z
 
-#if string matches 
+#.scan array with results === must include every char a-z
+
+
+  
+    # if no_space.scan(/[a-z]/).join === ''
+    #   return false
+    # else
+    #   return true
+    # end
+    
+      # result = true if result.scan(/[a-z]/) // fails #1, passes #2, #3
